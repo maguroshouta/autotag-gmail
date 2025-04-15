773bcdf3d4d3e893a2687f9dc4d3264ae80c80cf
maguroshouta <maguroshouta@gmail.com>
2025-04-15 16:23:39 +0900

    sleep

--- src/content.ts
+++ src/content.ts
@@ -32,8 +32,13 @@ function searchMailElement(): Promise<Node> {
 	});
 }
 
+function sleep(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 (async () => {
 	const mail_element = await searchMailElement();
+	await sleep(1500);
 
 	const mail_elements = mail_element.childNodes;
 
